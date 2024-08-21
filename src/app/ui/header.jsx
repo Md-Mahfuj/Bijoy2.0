a181203eccb87b08d634f1c1f351683c83079cd6
Md-Mahfuj <mdraju0162735@gmail.com>
2024-08-21 13:36:57 +0600

    implement Google Sign

--- src/app/ui/header.jsx
+++ src/app/ui/header.jsx
@@ -1,27 +1,45 @@
-"use client";
 import Image from "next/image";
+import LoginForm from "./LoginForm";
+import { auth } from "@/auth";
+import Logout from "./LogoutForm";
 
 import bijoy2024 from "../../../public/logo.jpg";
 
-const WithoutLogin = () => {
+const WithoutLogin = ({ session }) => {
   return (
     <div className="flex flex-1 items-center justify-end gap-x-6">
-      <a
-        href="#"
-        className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900"
-      >
-        Log in
-      </a>
-      <a
-        href={`/signup`}
-        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Sign up
-      </a>
+      {session?.user ? (
+        <div className="flex flex-col  justify-center items-center">
+          <Image
+            src={session?.user?.image}
+            alt={session?.user?.name}
+            width={42}
+            height={42}
+            className=" w-8 h-8 rounded-full"
+          />
+          <h1 className="text-xl">{session?.user?.name}</h1>
+        </div>
+      ) : (
+        <LoginForm />
+      )}
+
+      {session?.user ? (
+        <Logout />
+      ) : (
+        <a
+          href={`/signup`}
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Sign up
+        </a>
+      )}
     </div>
   );
 };
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+
+  // if (!session?.user) redirect("/");
   return (
     <header className="bg-white">
       <div className=" shadow-md fixed z-50 w-full bg-white items-center">
@@ -41,7 +59,7 @@ export default function Header() {
           </div>
 
           <div className=" lg:ml-4 flex items-center">
-            <WithoutLogin />
+            <WithoutLogin session={session} />
           </div>
         </div>
       </div>
